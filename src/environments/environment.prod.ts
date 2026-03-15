c4d627fe2776d9c372387e51eb70824751d5fad7
Daniel Almada <dannyalmada.94@gmail.com>
2026-03-15 00:26:53 -0300

    build: track firebase environment files for hosting

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD2enK1ktyiKFo8lewA_qFRjcdXvIKyaGg',
+    authDomain: 'logisticappdb.firebaseapp.com',
+    projectId: 'logisticappdb',
+    storageBucket: 'logisticappdb.firebasestorage.app',
+    messagingSenderId: '19347821428',
+    appId: '1:19347821428:web:d93c46a2e9bf0eeab4d221',
+    measurementId: 'G-ECGS9FSNMY',
+    databaseURL: 'https://logisticappdb-default-rtdb.firebaseio.com',
+  },
+};
